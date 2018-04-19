f058517e8844c17130979f75535f506fa1326782
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2018-04-19 14:20:54 +0200

    add clion file

---
+++ cmake-build-debug/CMakeFiles/nibbler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nibbler.dir/project/srcs/main.c.o"
+  "nibbler.pdb"
+  "nibbler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nibbler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
