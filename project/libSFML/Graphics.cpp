7afbee6dd6217d6a4187b3226bbe8e036f03d3bc
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2018-04-19 17:12:49 +0200

    gitignore

---
+++ project/libSFML/Graphics.cpp
@@ -0,0 +1,20 @@
+#include "Graphics.hpp"
+
+Graphics::Graphics() {
+
+}
+
+Graphics::Graphics(Graphics const &copy) {
+    *this = copy;
+}
+
+Graphics::~Graphics() {
+
+}
+
+Graphics &Graphics::operator=(Graphics const &copy) {
+    if (this != &copy) {
+        // copy 
+    }
+    return *this;
+}
---
+++ project/libVulkan/Graphics.hpp
@@ -0,0 +1,18 @@
+#ifndef NIBBLER_GRAPHICS_HPP
+#define NIBBLER_GRAPHICS_HPP
+
+class Graphics {
+
+    public:
+        Graphics(); // Canonical
+        Graphics(Graphics const &copy); // Canonical
+        ~Graphics(); // Canonical
+
+        Graphics &operator=(Graphics const &copy); // Canonical
+
+    private:
+
+};
+
+
+#endif //NIBBLER_GRAPHICS_HPP
