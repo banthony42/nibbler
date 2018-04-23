f11d45a51c23004981b0a432ba6302cc8158987b
Anthony BARA <banthony@e2r10p19.42.fr>
2018-04-23 14:42:23 +0200

    Avancement configure.sh

--- project/configure.sh
+++ project/configure.sh
@@ -6,14 +6,30 @@
 #    By: banthony <banthony@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/04/20 18:10:22 by banthony          #+#    #+#              #
-#    Updated: 2018/04/23 12:39:28 by banthony         ###   ########.fr        #
+#    Updated: 2018/04/23 14:39:39 by banthony         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# Initialise le fichier local de configuration
-git submodule init
+vulkan_tar="./libVulkan/vulkanLib.tar.gz"
+vulkan_src="./libVulkan/Vulkan"
+
 # Pull des submodule
+git submodule init
 git submodule update
+
+#Install OpenGL and dep
+brew install pkg-config && brew install glfw && brew install glew
+
+#Dl Vulkan sources if necessary
+if [ -f "$vulkan_tar" ]
+then
+	echo "$vulkan_tar found."
+else
+	curl https://sdk.lunarg.com/sdk/download/1.0.69.0/mac/vulkansdk-macos-1.0.69.0.tar.gz > $vulkan_tar
+fi
+
+mkdir $vulkan_src
+tar -xzf $vulkan_tar -C $vulkan_src --strip-components=1
 
 # Compilation de la Lib
 (cd ./LibSFML/SFML && cmake . && make -j4);
--- project/srcs/main.cpp
+++ project/srcs/main.cpp
@@ -6,13 +6,81 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 14:08:52 by cchameyr          #+#    #+#             */
-/*   Updated: 2018/04/19 14:09:48 by cchameyr         ###   ########.fr       */
+//   Updated: 2018/04/23 14:36:29 by banthony         ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../incl/nibbler.hpp"
+//#include <GL/gl.h>
+#include <OpenGL/gl.h>
+
+#include <GLUT/glut.h>
+
+void display(void) {
+
+	//clear white, draw with black
+	glClearColor(255, 255, 255, 0);
+	glColor3d(0, 0, 0);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//this draws a square using vertices
+	glBegin(GL_QUADS);
+	glVertex2i(0, 0);
+	glVertex2i(0, 128);
+	glVertex2i(128, 128);
+	glVertex2i(128, 0);
+	glEnd();
+
+	//a more useful helper
+	glRecti(200, 200, 250, 250);
+
+	glutSwapBuffers();
+
+}
+
+void reshape(int width, int height) {
+
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//set the coordinate system, with the origin in the top left
+	gluOrtho2D(0, width, height, 0);
+	glMatrixMode(GL_MODELVIEW);
+
+}
+
+void idle(void) {
+
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv) {
+
+	//a basic set up...
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(640, 480);
+
+	//create the window, the argument is the title
+	glutCreateWindow("GLUT program");
+
+	//pass the callbacks
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+
+	//we never get here because glutMainLoop() is an infinite loop
+	return 0;
+
+}
+
+/*#include "../incl/nibbler.hpp"
 
 int		main(int argc, char *argv)
 {
 	return (0);
 }
+*/
