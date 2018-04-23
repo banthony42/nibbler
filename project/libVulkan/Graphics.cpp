de4553be5f85fbeed5b26e89b1e998e2e67c817e
Anthony BARA <banthony@e2r10p19.42.fr>
2018-04-23 16:40:44 +0200

    Avancement compilation Vulkan

--- project/libVulkan/Graphics.cpp
+++ project/libVulkan/Graphics.cpp
@@ -1,5 +1,15 @@
 #include "Graphics.hpp"
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <vulkan/vulkan.h>
+
 Graphics::Graphics() {
 
 }
@@ -22,4 +32,26 @@ Graphics &Graphics::operator=(Graphics const &copy) {
 void	Graphics::helloWorld(void) {
 // vulkan
 	std::cout << "Hello Vulkan" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported" << std::endl;
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
 }
